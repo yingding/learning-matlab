8a536627177c723fa0bf57ba19acfcafe341b671
Yingding Wang <yingdingwang@wailagi.fritz.box>
2016-01-04 18:46:58 +0100

    add tutorial computing data

---
+++ tutorial06_computing_data.m
@@ -0,0 +1,24 @@
+clear, close all, clc
+% This section demonstrate the computing of data in matlab / Octabe
+A = [1 2; 3 4; 5 6];
+B = [11 12; 13 14; 15 16];
+
+% Element-wise multiplication with .* 
+A .* B
+% Element-wise squaring 
+A .^ 2
+
+% Element-wise reciprocal of v
+v = [1; 2; 3];
+1 ./ v
+
+% Element-wise natural logrithm
+log(v)
+% Element-wise logrithm base 10
+log10(v)
+
+% Element-wise natural exponential
+exp(v)
+
+
+
